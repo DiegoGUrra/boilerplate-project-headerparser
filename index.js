dca8ece77ad5d908b82917e7937256e298d60b88
DiegoGUrra <clicknamk@gmail.com>
2022-09-25 20:28:42 -0300

    added json with ip languague and software

--- index.js
+++ index.js
@@ -24,9 +24,8 @@ app.get('/', function (req, res) {
 app.get('/api/whoami', function (req, res) {
   console.log('Headers: ' + JSON.stringify(req.headers));
   console.log('IP: ' + JSON.stringify(req.ip));
-  console.log("Browser: " + req.headers["user-agent"]);
-  console.log("Language: " + req.headers["accept-language"]);
-  res.json({ greeting: 'hello API' });
+  res.json({ "ipaddress":req.headers["x-forwarded-for"],"language":req.headers["accept-language"],"software":req.header["user-agent"] });
+
 });
 
 // listen for requests :)
